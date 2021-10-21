f2feb4c86e137e46a5a28578db43b7a322fe82be
S0han <43890209+S0han@users.noreply.github.com>
2021-10-21 01:41:09 -0400

    node mongodb

---
+++ app.js
@@ -0,0 +1,101 @@
+// var MongoClient = require('mongodb').MongoClient;
+
+// //Connect to the db
+
+// MongoClient.connect("mongodb://localhost:27017/MyDb", function (err, db) {
+
+//     if (err) {
+//         throw err;
+//     }
+
+//     //Write database Insert/Update/Query code here..
+
+// });
+
+
+
+//****************************************************************************** */
+
+
+
+// var MongoClient = require('mongodb').MongoClient;
+
+// //Connect to the db
+// MongoClient.connect("mongodb://localhost:27017/MyDb", function(err,db) {
+    
+//     db.collection('Persons', function (err, collection) {
+
+//         collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs'});
+//         collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates'});
+//         collection.insert({ id: 3, firstName: 'James', lastName: 'Bond'});
+
+//         db.collection('Persons').count(function (err, count) {
+
+//             if (err) {
+//                 throw err;
+//             }
+
+//             console.log('Total Rows: ' + count);
+//         });
+//     });
+
+// });
+
+//***************************************************************** */
+
+
+// var MongoClient = require('mongodb').MongoClient;
+
+// //connect to the db
+
+// MongoClient.connect("mongodb:localhost27017/MyDb", function (err, db) {
+
+//     db.collection('Persons', function (err, collection) {
+
+//         collection.update( {id: 1}, { $set: { firstName: 'James', lastName: 'Gosling'} }, {w: 1},
+//                                                     function(err, result) {
+//                                                                 if (err) {
+//                                                                     throw err;
+//                                                                 }
+//                                                                 console.log('Document Updated Successfully');
+//                                                     });
+        
+//         collection.remove({id: 2}, {w: 1}, function (err, result) {
+
+//             if (err) {
+//                 throw err;
+//             }
+
+//             console.log('Document Removed Successfully');
+
+//         });
+
+//     });
+
+// });
+
+//******************************************************************************************************************* */
+
+var MongoClient = reqiure('mongodb').MongoClient;
+
+//Connect to the db
+MongoClient.connect("mongodb://localhost:27017/MyDb", function (err, db) {
+
+    db.collection('Persons', function (err, collection) {
+
+        collection.find().toArray(function(err, items) {
+            if (err) {
+                throw err;
+            }
+            console.log(items);
+        });
+        
+    });
+
+});
+
+
+
+
+
+
